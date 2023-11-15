6bc2aadafad43bc7a5a6246b998eb94b1cc919f9
Ubits <jesanchez@ubits.co>
2023-11-15 16:07:30 -0500

    test commit

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "../pages/home/home";
+
+
+
+const Routes =()=> {
+
+
+
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Home} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes
